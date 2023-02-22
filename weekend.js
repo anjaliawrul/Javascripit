f50f89097f00052e61b97bbdb201e49d6bf25c8b
anjaliawrul <111339784+anjaliawrul@users.noreply.github.com>
2023-02-22 13:36:08 +0530

    Add files via upload

---
+++ weekend.js
@@ -0,0 +1,26 @@
+// let readlinesync=require("readlinesync");
+// var a1=readlinesync.question("enter the weekend")
+
+if(a1==1){
+    console.log("sunday")
+}
+else if (a1==2){
+    console.log("monday")
+}
+else if(a1==3){
+    console.log("tuesday")
+}
+else if(a1==4){
+    console.log("wednesday")
+}
+else if(a1==5){
+    console.log("thrusday")
+}
+else if(a1==6){
+    console.log("friday")
+}
+else if(a1==7){
+    console.log("saturday")
+}
+else{
+    console.log("nothing")
